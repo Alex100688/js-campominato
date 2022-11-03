0cd09254d8272e9e828c2cde2039fc955015615b
Alex100688 <alessiozingaro2@gmail.com>
2022-11-03 14:57:52 +0100

    Aggiunge una funzione per i numeri random

--- js/main.js
+++ js/main.js
@@ -1,17 +1,23 @@
 "use strict";
+//function//
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
 const buttonPlay=document.querySelector(".btn-play");
 buttonPlay.addEventListener('click', function(){
     window.location.reload();
 })
-
 const campoBox=document.querySelector(".campo-box");
 for (let i = 1; i <=100; i++) {
     const boxCell=document.createElement("div");
     boxCell.innerHTML=i;
     boxCell.classList.add("cell-box");
+    const cpuNumber=document.querySelector(".cpu-number");
+    const cpuRandom=getRndInteger(1, 16);
+    cpuNumber.innerHTM=cpuRandom;
+    console.log(cpuRandom, cpuNumber);
     boxCell.addEventListener('click', function(){
-        this.classList.add("number-green");
-        this.classList.toggle("number-red")
+        
        
     });
     campoBox.append(boxCell);
